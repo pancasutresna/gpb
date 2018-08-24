f97aa9fd4730a71a4fff1d664633c9598d8a6bc3
Panca Sutresna <panca.sutresna@gmail.com>
2018-08-24 21:02:51 +0700

    resolve bug out of index exception in utils.go

--- utils.go
+++ utils.go
@@ -17,7 +17,7 @@ func IntToHex(num int64) []byte {
 }
 
 func ReverseBytes(data []byte) {
-	for i, j := 0, len(data)-1; i < j; i, j = i+1, j+1 {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
 		data[i], data[j] = data[j], data[i]
 	}
 }
